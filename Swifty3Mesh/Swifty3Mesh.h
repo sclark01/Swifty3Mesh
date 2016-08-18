0fd9db7cbdd6b06134baa17348be9b656134d785
Shea <sclark.tieche@gmail.com>
2016-08-18 09:50:29 -0700

    initial commit

---
+++ Swifty3Mesh/Swifty3Mesh.h
@@ -0,0 +1,19 @@
+//
+//  Swifty3Mesh.h
+//  Swifty3Mesh
+//
+//  Created by Shea Clark-Tieche on 8/18/16.
+//  Copyright © 2016 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swifty3Mesh.
+FOUNDATION_EXPORT double Swifty3MeshVersionNumber;
+
+//! Project version string for Swifty3Mesh.
+FOUNDATION_EXPORT const unsigned char Swifty3MeshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swifty3Mesh/PublicHeader.h>
+
+
